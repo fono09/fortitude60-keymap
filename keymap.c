0d95ea6b143cf5efac24eb4af03e614f1b017abe
fono09 <fono@fono.jp>
2020-04-21 19:56:22 +0900

    一日使って辛かったところを変える

--- keymap.c
+++ keymap.c
@@ -41,7 +41,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                   KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL, \
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
   KC_LCTL,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                   KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC, KC_RBRC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT , \
+  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC, KC_RBRC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT, \
                  KC_LGUI, KC_LALT,   LOWER,  KC_SPC, KC_ESC,  KC_ENT, KC_RSFT, RAISE,    KC_RALT, KC_RGUI\
 ),
 
@@ -49,20 +49,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------.             ,-----------------------------------------.
  * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |             |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |
  * |------+------+------+------+------+------|             |------+------+------+------+------+------|
- * |   ~  |   !  |   @  |   #  |   $  |   %  |             |   ^  |   &  |   *  |   (  |   )  |  \   |
+ * |   `  |   1  |   2  |   3  |   4  |   5  |             |   6  |   7  |   8  |   9  |   0  |  \   |
  * |------+------+------+------+------+------|             |------+------+------+------+------+------|
- * |  Esc |  F1  |  F2  |  F3  |  F4  |  F5  |             |  F6  |   _  |   +  |   [  |   ]  |Enter |
+ * | Z/H  |   !  |   @  |   #  |   $  |   %  |             |   ^  |   &  |   *  |   (  |   )  |Enter |
  * |------+------+------+------+------+------+-------------+------+------+------+------+------+------|
- * |      |  F7  |  F8  |  F9  |  F10 |  F11 |      |      |  F12 |ISO ~ |ISO | |      |      |      |
+ * |      |      |      |      |      |      |      |      |      |   -  |   +  |   [  |   ]  | Ctrl |
  * `-------------+------+------+------+------+------+------+------+------+------+------+-------------'
  *               |      |      |      |      |      |      |      |      |      |      |
  *               `---------------------------------------------------------------------'
  */
 [_LOWER] = LAYOUT( \
-  KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,                   KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, \
-  KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSLS,  \
-  KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_UNDS, KC_PLUS, KC_LBRC, KC_RBRC, KC_ENT, \
-  _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  _______, _______, KC_F12,S(KC_NUHS),S(KC_NUBS),_______, _______, _______,\
+  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, \
+  KC_GRV,  KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSLS, \
+  A(KC_GRV),KC_EXLM,KC_AT,  KC_HASH, KC_DLR,  KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_ENT,  \
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_RCTL,
                     _______, _______, _______, _______, _______, _______,  _______, _______, _______, _______\
 ),
 
@@ -72,18 +72,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------.             ,------+------+------+------+------+------|
  * |   `  |   1  | PGUP |   3  |   4  |   5  |             |   6  |   7  |   8  |   9  |   0  | Bksp |
  * |------+------+------+------+------+------.             ,------+------+------+------+------+------|
- * | Del  | HOME | PGDN |  END |  F4  |  F5  |             | LEFT | DOWN |  UP  | RIGHT|   ]  |  |   |
+ * | Del  | HOME | PGDN |  END |  F4  |  F5  |             | LEFT | DOWN |  UP  | RIGHT|   /  |  |   |
  * |------+------+------+------+------+------+-------------+------+------+------+------+------+------|
- * |      |  F7  |  F8  |  F9  |  F10 |  F11 |      |      |  F12 |ISO # |ISO / |      |      |      |
+ * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |      |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |
  * `-------------+------+------+------+------+------+------+------+------+------+------+-------------'
  *               |      |      |      |      |      |      |      |      |      |      |
  *               `---------------------------------------------------------------------'
  */
 [_RAISE] = LAYOUT( \
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                       KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,  \
-  KC_GRV,  KC_1,    KC_PGUP,   KC_3,    KC_4,    KC_5,                       KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
-  KC_DEL,  KC_HOME, KC_PGDN, KC_END, KC_F4,   KC_F5,                      KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_RBRC,    KC_BSLS, \
-  _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  _______, _______,  KC_F12,  KC_NUHS, KC_NUBS, _______, _______, _______, \
+  KC_GRV,  KC_1,    KC_PGUP, KC_3,    KC_4,    KC_5,                       KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
+  KC_DEL,  KC_HOME, KC_PGDN, KC_END,  KC_F4,   KC_F5,                      KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_SLSH, KC_PIPE, \
+  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   _______, _______,  KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, \
                     _______, _______, _______, _______, _______, _______,  _______, _______, _______, _______\
 ),
 
